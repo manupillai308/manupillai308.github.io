609d4624d9451a0285d51c5ed5ae204575acc65b
manupillai308 <manupillai308@gmail.com>
2022-06-10 21:13:52 +0530

    completed publications.html

--- scripts/loader.js
+++ scripts/loader.js
@@ -10,7 +10,9 @@ function loadMain(){
     fetch(endpointUrl+"/aboutme")
     .then((res) => res.json())
     .then((d) => {
-        document.getElementById("aboutme-image").style.backgroundImage = `url(${d["image"]})`;
+        const img = document.getElementById("aboutme-image");
+        img.style.backgroundImage = `url(${d["image"]})`;
+        img.innerHTML = "";
         document.getElementById("aboutme-body").innerHTML = d["body"];
     });
 
@@ -24,4 +26,69 @@ function loadMain(){
             newspad.insertBefore(newsNode, refnode);
         }
     })
+    fetch(endpointUrl+"/carousel")
+    .then(res => res.json())
+    .then(data => {
+        const carousel = document.getElementById("carousel");
+        const dot = document.getElementById("dot");
+        carousel.removeChild(carousel.firstElementChild);
+        dot.removeChild(dot.firstElementChild);
+        let i = 1, t = Object.entries(data).length;
+        for (const value of Object.values(data)){
+            const carNode = createNodeFromHTML(`<div class="mySlides fade">
+            <div class="numbertext">${i} / ${t}</div>
+            <div class="car-image box-shadow" style="background-image: url(${value["image"]});"></div>
+            <div class="text">${value["caption"]}</div>
+        </div>`);
+            carousel.insertBefore(carNode, dot); i++;
+            const newDot = createNodeFromHTML(`<span class="dot"></span>`);
+            dot.appendChild(newDot);
+        }
+    })
+
+
 }
+
+function loadPub(){
+    const publist = `<div class="pub-list">
+        <div class="pub-title">$$TITLE$$$$NEW$$</div>
+        <div class="pub-authors">$$AUTHORS$$</div>
+        <div style="display: flex; margin: 0.3rem 0; align-items: center;">
+        </div></div>`;
+    function pubLoader(what){
+        fetch(endpointUrl+`/publications?which=${what}`)
+        .then((res) => res.json())
+        .then(data => {
+            const which = document.getElementById(`${what}`);
+            // authors, date, doi, paper, project, talk, title, code, new
+            for (const value of Object.values(data)){
+                var publist_html = publist.replace("$$TITLE$$", value["title"]).replace("$$AUTHORS$$", value["authors"].trim());
+                if (value["new"]) publist_html = publist_html.replace("$$NEW$$", ` <span class="pulsate">NEW!</span>`);
+                else publist_html = publist_html.replace("$$NEW$$", ``);
+
+                delete value["authors"];
+                delete value["title"];
+                delete value["new"];
+                const pubNode = createNodeFromHTML(publist_html);
+                which.appendChild(pubNode);
+
+                const metaNode = pubNode.lastChild;
+                if(value["doi"]){
+                    metaNode.appendChild(createNodeFromHTML(`<div onclick="window.open('${value['doi']}', '_blank')" class="pub-meta">DOI</div>`));
+                    delete value["doi"];
+                }
+                if(value["doi"] == "") delete value["doi"];
+                for (const [key, val] of Object.entries(value)){
+                    if((key === "date") || val == "") continue;
+                    metaNode.appendChild(createNodeFromHTML(`<div onclick="window.open('${val}', '_blank')" class="pub-meta">${key}</div>`));
+                }
+                metaNode.appendChild(createNodeFromHTML(`<div class="pub-date">${value["date"]}</div>`));
+            }
+        });
+    }
+    pubLoader("journal");
+    pubLoader("conference");
+    pubLoader("bookchapter");
+}
+
+{/* <span class="pulsate">NEW!</span> */}
